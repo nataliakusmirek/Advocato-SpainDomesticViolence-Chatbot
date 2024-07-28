c43eb8d0955e2d1a064ecf72223938da6e9b7756
Natalia Kusmirek <nekusmirek@wpi.edu>
2024-07-28 13:28:40 -0400

    Django web work
    index page and generic parts of each subpage created and formatted

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('spain_form/', views.spain_form, name='spain_form'),
+    path('future/', views.future, name='future'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('story/', views.story, name='story'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def spain_form(request):
+    return render(request, 'spain_form.html')
+
+def future(request):
+    return render(request, 'future.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def story(request):
+    return render(request, 'story.html')
